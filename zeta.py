0f086d65a852a85c4cfed5770c4975c3fc429924
lktn <140223005+lktn@users.noreply.github.com>
2024-08-24 14:20:09 +0700

    Update zeta.py

--- zeta.py
+++ zeta.py
@@ -13,18 +13,12 @@ class ComplexFunctionPlot(Scene):
             y_length=16*a/9
         ).add_coordinates(font_size=45)
         
-        def complex_function(z):
-            return zeta(0.5 + 1j * z)
+        def func(t):
+            return complex_plane.n2p(zeta(0.5 + 1j * t))
         
-        def parametric_function(t):
-            z = complex(t, 0)
-            return complex_plane.n2p(complex_function(z))
-        
-        function_graph = ParametricFunction(parametric_function, t_range=[0, 100]).set_stroke([YELLOW, BLUE])
+        graph = ParametricFunction(func, t_range=[0, 100]).set_stroke([YELLOW, BLUE])
         b = MathTex(r"\zeta(s)=\sum_{n=1}^{\infty}\frac{1}{n^s}").move_to(UP*8).scale(1.7)
         
-        self.add(complex_plane, b)
-        self.wait(1)
-        self.play(Create(function_graph, rate_func=linear, run_time=15))
-        self.wait(1)
+        self.add(complex_plane, b, graph)
+        self.play(Create(graph, rate_func=linear, run_time=15))
         self.play(FadeOut(VGroup(b, function_graph, complex_plane, stroke_color=BLUE), run_time=1))
